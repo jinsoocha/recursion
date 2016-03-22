db2b9d8a2b4ca5bbd1785aab6807d001e5e7e449
Jinsoo Cha <jinscha@indiana.edu>
2016-03-22 14:03:29 -0700

    "Completed stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,41 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(obj===null) {
+    return 'null'
+  }
+  else if(obj===undefined || typeof obj==='function') {
+        
+  }
+  else if(obj.constructor===Array) {
+    var newobj="[";
+    for(var i=0; i<obj.length; i++) {
+      newobj=newobj+stringifyJSON(obj[i])+',';
+    }
+    if(newobj.length>1) {
+      newobj=newobj.slice(0,-1);
+    }
+    return newobj+"]";
+  }
+  else if(obj.constructor===Object) {
+  	var newobj='{';
+    for(var key in obj) {
+      if(obj[key]===undefined || typeof obj[key]==='function') {
+        newobj=newobj;
+      }
+      else {
+      newobj=newobj+'"'+key+'":'+stringifyJSON(obj[key])+',';
+      }
+    }
+    if(newobj.length>1) {
+      newobj=newobj.slice(0,-1);
+    }
+    return newobj=newobj+'}';
+  }    
+  else if(obj.constructor===String) {
+    return '"'+obj+'"';
+  }
+  else {
+    return ''+obj;
+  }
 };
