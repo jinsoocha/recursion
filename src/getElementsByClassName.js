df31b0630da6648ae430523b7c35737e02cbbffd
Jinsoo Cha <jinscha@indiana.edu>
2016-03-22 20:07:16 -0700

    "Completed getElmentsByClassName"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
+  var hasclass = [];
+  var body = document.body;
   // your code here
+  if(body.classList.contains(className)) {
+  	hasclass.push(body);
+  }
+  var recursive = function(element) {
+    if(element.childNodes.length>0) {
+      for(var i=0; i<element.childNodes.length; i++) {
+        if(element.childNodes[i].classList!==undefined) {
+          if(element.childNodes[i].classList.contains(className)) {
+            hasclass.push(element.childNodes[i]);
+          }
+        }
+      recursive(element.childNodes[i]); 
+      };
+    };
+  };
+  recursive(body);
+  return hasclass;
 };
